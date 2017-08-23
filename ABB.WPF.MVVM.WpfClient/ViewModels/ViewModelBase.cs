dfddd7288deae040c35d56a1b86baa8754e66e9b
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-08-23 11:31:02 +0200

    Utworzenie bazowej klasy ViewModel

---
+++ ABB.WPF.MVVM.WpfClient/ViewModels/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using ABB.WPF.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABB.WPF.MVVM.WpfClient.ViewModels
+{
+    public abstract class ViewModelBase : Base
+    {
+    }
+}
