91d7fdfb75fafe3ea55c0d13617260dc09c48d27
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-08-23 11:32:40 +0200

    Utworzenie serwisów

---
+++ ABB.WPF.MVVM.Services/MockMenuService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABB.WPF.MVVM.Models;
+
+namespace ABB.WPF.MVVM.Services
+{
+    public class MockMenuService : IMenuService
+    {
+        public Menu Get()
+        {
+            var menu = new Menu
+            {
+                MenuItems = new List<MenuItem>
+                {
+                    new MenuItem
+                    {
+                        MenuItemId = 1,
+                        Name = "OMS",
+                        Caption = "OMS"
+                    },
+
+                    new MenuItem
+                    {
+                        MenuItemId = 2,
+                        Name = "GMS",
+                        Caption = "GMS"
+                    },
+
+                    new MenuItem
+                    {
+                        MenuItemId = 3,
+                        Name = "EMS",
+                        Caption = "EMS"
+                    },
+
+                }
+            };
+
+            return menu;
+        }
+    }
+}
