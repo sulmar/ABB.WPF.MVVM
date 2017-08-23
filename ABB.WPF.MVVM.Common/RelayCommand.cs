c99d0fb45f531f45e45432a9b94a0d7421393579
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-08-23 11:29:20 +0200

    Dodanie RelayCommand

---
+++ ABB.WPF.MVVM.Common/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ABB.WPF.MVVM.Common
+{
+
+    public class RelayCommand : RelayCommand<object>
+    {
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null) 
+            : base(execute, canExecute)
+        {
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private Action<T> execute;
+        private Predicate<T> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => canExecute == null || canExecute((T)parameter);
+
+        public void Execute(object parameter = null) => execute((T)parameter);
+    }
+}
---
+++ ABB.WPF.MVVM.Common/RelayCommandOld.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ABB.WPF.MVVM.Common
+{
+    public class RelayCommandOld : ICommand
+    {
+        private Action<object> execute;
+        private Predicate<object> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+
+        public RelayCommandOld(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => canExecute == null || canExecute(parameter);
+
+        public void Execute(object parameter = null) => execute(parameter);
+    }
+}
