66e3bcc718ccc77f9df8fbe4a7d561aae018862c
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-08-24 15:00:22 +0200

    Dodanie nagivation service

---
+++ ABB.WPF.MVVM.Common/INavigationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABB.WPF.MVVM.Common
+{
+    public interface INavigationService
+    {
+        bool Navigate<T>(object parameter = null);
+
+        bool Navigate(string page, object parameter = null);
+
+        object Parameter { get;  }
+    }
+}
---
+++ ABB.WPF.MVVM.Common/NavigationService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ABB.WPF.MVVM.Common
+{
+    public class NavigationService : INavigationService
+    {
+        public Frame frame { get; set; }
+
+        public NavigationService(Frame frame = null)
+        {
+            this.frame = frame;
+        }
+
+        public object Parameter { get; private set; }
+
+
+        // Navigate<PageView>()
+
+        public bool Navigate<T>(object parameter = null)
+        {
+            return Navigate(typeof(T), parameter);
+        }
+
+        private bool Navigate(Type source, object parameter = null)
+        {
+            // Zapamiętujemy przekazany parametr
+            Parameter = parameter;
+
+            // new w runtime'ie
+            var src = Activator.CreateInstance(source);
+
+            // Przechodzimy do okna
+            return frame.Navigate(src, parameter);
+
+        }
+
+        public bool Navigate(string page, object parameter = null)
+        {
+            // Szukamy typu w naszej aplikacji na podstawie nazwy
+            var type = Assembly.GetEntryAssembly()
+                .GetTypes()
+                .SingleOrDefault(a => a.Name.Equals(page));
+
+            if (type == null) return false;
+
+            return Navigate(type, parameter);
+
+        }
+    }
+}
