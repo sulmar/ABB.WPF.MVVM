e513489a3da152e880c25a074bb2d8ef52d83bf9
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-08-24 15:02:00 +0200

    Dodanie PageViewModel

---
+++ ABB.WPF.MVVM.WpfClient/ViewModels/PageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABB.WPF.MVVM.Common;
+using ABB.WPF.MVVM.Services;
+using ABB.WPF.MVVM.Models;
+
+namespace ABB.WPF.MVVM.WpfClient.ViewModels
+{
+    public class PageViewModel : ViewModelBase
+    {
+        public PageContent PageContent { get; set; }
+
+        private readonly IPageContentService pageContentService;
+
+        public PageViewModel(INavigationService navigationService, IPageContentService pageContentService) 
+            : base(navigationService)
+        {
+            this.pageContentService = pageContentService;
+
+            Load();
+        }
+
+        private void Load()
+        {
+            MenuItem menuItem = (MenuItem)navigationService.Parameter;
+
+            PageContent = pageContentService.Get(menuItem.Name);
+
+            // Podpinamy notyfikację do wszystkich elementów
+            Subscribe();
+        }
+
+        private void Subscribe()
+        {
+            foreach (var pageItem in PageContent.Items)
+            {
+                pageItem.PropertyChanged += PageItem_PropertyChanged;
+            }
+        }
+
+        private void PageItem_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName ==  "Value")
+            {
+                // TODO: recalculate or save
+            }
+        }
+    }
+}
