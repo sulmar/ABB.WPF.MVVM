d6159049715565eecfd5780782bbe3120440114d
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-08-24 15:02:39 +0200

    Zmiany w widokach

--- ABB.WPF.MVVM.WpfClient/Views/ShellView.xaml.cs
+++ ABB.WPF.MVVM.WpfClient/Views/ShellView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ABB.WPF.MVVM.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,7 @@ namespace ABB.WPF.MVVM.WpfClient.Views
             InitializeComponent();
 
           
+
         }
     }
 }
