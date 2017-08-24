b55975157e2a974793f3bedef209c9d03cf24456
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-08-24 15:02:27 +0200

    Dodanie locatora

---
+++ ABB.WPF.MVVM.WpfClient/ViewModelLocator.cs
@@ -0,0 +1,16 @@
+﻿using ABB.WPF.MVVM.Services;
+using ABB.WPF.MVVM.WpfClient.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABB.WPF.MVVM.WpfClient
+{
+    public class ViewModelLocator
+    {
+        public ShellViewModel ShellViewModel => new ShellViewModel(App.NavigationService, new MockMenuService());
+        public PageViewModel PageViewModel => new PageViewModel(App.NavigationService, new FilePageContentService());
+    }
+}
