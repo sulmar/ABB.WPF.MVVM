ab40c88eefa892be3953f7732419ad450e104e44
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-08-24 15:02:13 +0200

    Dodanie selektora

---
+++ ABB.WPF.MVVM.WpfClient/PageItemDataTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using ABB.WPF.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ABB.WPF.MVVM.WpfClient
+{
+    public class PageItemDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var pageItem = (PageItem)item;
+
+            var resourceName = $"{pageItem.Type}Template";
+
+            // Ładowanie zasobu z kodu
+            DataTemplate dataTemplate = Application.Current.FindResource(resourceName) as DataTemplate;
+
+            return dataTemplate;
+        }
+    }
+}
